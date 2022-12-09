37d70a3a6d9beeda1249b59d7d923fb81d0c4dca
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-08 16:27:25 -0800

    f

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main- severs as the entry point of the program
+ * while return is the end
+ */
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
