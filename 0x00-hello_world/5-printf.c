0b25f6a7531b5902ad3bf7b1966041f44a1b810c
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-08 16:42:26 -0800

    d

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- the entry point for code
+ *
+ * Return: o if error, no-zero if error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
