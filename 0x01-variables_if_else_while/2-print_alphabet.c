dffc896eff5739717ba49c347da465bfaee931c3
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-09 17:54:53 -0800

    d

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main- entry point for program
+ *
+ *Return: zero or non-zero value
+ */
+
+int main(void)
+{
+	/*Declarations goes here */
+	char alpha;
+
+	/*initialization goes here */
+	alpha = 'a';
+
+	/*Code goes here */
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	return (0);
+
+}
