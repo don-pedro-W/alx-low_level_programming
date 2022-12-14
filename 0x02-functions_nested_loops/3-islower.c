20d87030cfcda5c72572586edaa00ac1737fbff0
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 17:12:52 -0800

    r

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if lowercase, else returns 0
+ *
+ * Return - on success 1
+ */
+
+int _islower(int c)
+{
+	int r = 0,i;
+	for (i = 61; i < 123; i++)
+	{
+		if (c == i)
+		{
+			r = 1;
+			break;
+		}
+	}
+
+	return r;
+}
+
