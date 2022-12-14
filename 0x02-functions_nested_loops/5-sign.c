82c2551f486fdd9a7768a3db5bce0cbae77ba610
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 17:55:13 -0800

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign-checkthe sign of a number
+ * @n: Number to check
+ *
+ * Return: 1-if n is greater than 0: 0-if equal and -1 if less
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n > 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
