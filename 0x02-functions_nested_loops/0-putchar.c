835facc0a72d5ebcb6947a80bcc5e751ba413104
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 16:10:18 -0800

    h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *main- entry point for program
+ *
+ *Return: zero or non-zero value
+ */
+int main(void)
+{
+
+	/*Declarations goes here */
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	/*initialization goes here */
+
+	/*Code goes here */
+
+	return (0);
+
+}
