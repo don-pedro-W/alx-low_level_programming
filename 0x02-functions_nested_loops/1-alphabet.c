719683dc59a2da7fda5b7a28e7f2321e65efe4e5
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 16:41:39 -0800

    f

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints all alphabet
+ *
+ * Return: 1 if error, 0 if none
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a ='a'; a <='z'; a++)
+		_putchar(a);
+	_putchar('\n');
+
+}
