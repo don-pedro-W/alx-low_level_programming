3ada167fdd68a9a264ef9b71457d67a159783de4
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 16:54:04 -0800

    s

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,13 @@
 void print_alphabet_x10(void)
 {
 	char a;
+	int i;
 
-	for (a = 'a'; a <= 'z'; a++)
-		_putchar(a);
-	_putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+		_putchar('\n');
+	}
 
 }
