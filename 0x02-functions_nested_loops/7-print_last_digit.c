98305bfff2aa0024bc8125d6010af40df5c219c2
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-13 18:09:50 -0800

    k

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit-prints last digit of num
+ * @a: contains num
+ * Return: last digit
+ */
+
+int print_last_digit(int a)
+{
+	int last;
+	
+	last = a % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}	
