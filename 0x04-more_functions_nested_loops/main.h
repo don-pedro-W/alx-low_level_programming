00313f5e089f2db32924de75cf9cbfda5259c408
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-16 16:24:41 -0800

    j

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef main_h
 #define main_h
+extern int _putchar(int c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
+extern void print_numbers(void);
 #endif
