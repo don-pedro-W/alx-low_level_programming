7343e6eae92c7d055aa3e050c176bcf81cd2d3ae
Tonye Don-Pedro <donpedro.willz@gmail.com>
2022-12-22 11:12:08 -0800

    h

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -25,17 +25,17 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 
 	while (dest[i] != '\0')
-	{									i++;
+	{
+		i++;
 	}
-
 
 	j = 0;
 
 	while (j < n && src[j] != '\0')
 	{
-										dest[i] = src[j];				
-										i++;
-										j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 
 	}
 
