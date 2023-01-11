861d42d7ed20199708c5bb6b1057d44e5a6de187
Tonye Don-Pedro <donpedro.willz@gmail.com>
2023-01-10 17:06:55 -0800

    j

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 #include <stdlib.h>
-
-
 
 /**
  * str_concat - concatenates two strings.
@@ -14,15 +11,10 @@
  * Return: pointer of an array of chars
  *
  */
-
 char *str_concat(char *s1, char *s2)
-
 {
-
 	char *strout;
-
 	unsigned int i, j, k, limit;
-
 
 
 	if (s1 == NULL)
@@ -31,45 +23,28 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-
-
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-
-
 
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-
-
 	strout = malloc(sizeof(char) * (i + j + 1));
 
-
-
 	if (strout == NULL)
-
 	{
-
 		free(strout);
 
 		return (NULL);
 	}
 
-
-
 	for (k = 0; k < i; k++)
 		strout[k] = s1[k];
-
-
 
 	limit = j;
 
 	for (j = 0; j <= limit; k++, j++)
 		strout[k] = s2[j];
 
-
-
 	return (strout);
-
 }
