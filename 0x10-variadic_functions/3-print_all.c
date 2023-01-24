565996914e718149f383f568438f542703904674
Tonye Don-Pedro <donpedro.willz@gmail.com>
2023-01-24 15:53:48 -0800

    k

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -28,44 +28,29 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(valist, int)), c = 1;
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
 
-																													break;
+			case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
 
-																															case 'i':
+			case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
 
-																																printf("%d", va_arg(valist, int)), c = 1;
+			case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
 
-																																			break;
+			printf("%s", str);
+			break;
+		} i++;
 
-																																					case 'f':
-
-																																						printf("%f", va_arg(valist, double)), c = 1;
-
-																																									break;
-
-																																											case 's':
-
-																																												str = va_arg(valist, char *), c = 1;
-
-																																															if (!str)
-
-																																																			{
-
-																																																								printf("(nil)");
-
-																																																												break;
-
-																																																															}
-
-																																																		printf("%s", str);
-
-																																																					break;
-
-																																																							} i++;
-
-																	}
-
-								printf("\n"), va_end(valist);
-
+	}
+	printf("\n"), va_end(valist);
 }
