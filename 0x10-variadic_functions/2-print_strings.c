fd22536404320532ef0fafcb617946fab220bd83
Tonye Don-Pedro <donpedro.willz@gmail.com>
2023-01-24 15:58:44 -0800

    d

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 		else
 			printf("(nil)");
-		if (i < n -1)
+		if (i < n - 1)
 			if (separator)
 				printf("%s", separator);
 	}
